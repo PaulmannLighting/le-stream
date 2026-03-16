b6a6a4fd2148ea45230959b0b6e9a670af851324
Richard Neumann <neumann@paulmann.de>
2026-03-16 10:58:46 +0100

    Utilize size hint of iterator to optimize vector allocation.

--- le-stream/src/from_le_stream/alloc.rs
+++ le-stream/src/from_le_stream/alloc.rs
@@ -16,12 +16,14 @@ where
     where
         I: Iterator<Item = u8>,
     {
-        let mut result = Self::new();
+        let (lower, upper) = bytes.size_hint();
+        let mut result = Self::with_capacity(upper.unwrap_or(lower));
 
         while let Some(byte) = bytes.next() {
             result.push(T::from_le_stream(once(byte).chain(&mut bytes))?);
         }
 
+        result.shrink_to_fit();
         Some(result)
     }
 }
