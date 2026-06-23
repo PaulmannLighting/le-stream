bd2359dda75290f6afe8d55bbd194d14b0171f68
Richard Neumann <neumann@paulmann.de>
2026-06-23 18:10:42 +0200

    Remove obsolete derive macros

--- le-stream-derive/src/lib.rs
+++ le-stream-derive/src/lib.rs
@@ -1,25 +1,17 @@
 //! Derive macros for the `FromLeStream` and `ToLeStream` traits.
 
 use from_le_stream::from_le_stream;
-use from_le_stream_tagged::from_le_stream_tagged;
 use proc_macro2::Ident;
 use syn::{DeriveInput, GenericParam, Generics, TypeParamBound};
 use to_le_stream::to_le_stream;
 
 mod from_le_stream;
-mod from_le_stream_tagged;
 mod to_le_stream;
 
 /// Derive the `FromLeStream` trait for a struct.
 #[proc_macro_derive(FromLeStream)]
 pub fn derive_from_le_stream(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     from_le_stream(input)
-}
-
-/// Derive the `FromLeStreamTagged` trait for a struct.
-#[proc_macro_derive(FromLeStreamTagged)]
-pub fn derive_from_le_stream_tagged(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    from_le_stream_tagged(input)
 }
 
 /// Derive the `ToLeStream` trait for a struct.
